d6292b4147ae42a6c75fcf70d2cba67933637ad6
NickCyrus <nicklemusduque@gmail.com>
2020-08-12 20:39:14 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -599,6 +599,7 @@ app = {
         },
 
         is_movil : function(){
+            return true;
             var isMobile = false; 
             if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent.substr(0,4))) { 
             isMobile = true;
