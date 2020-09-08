fce9319a7e7fe1be395e0e52a51955bc38884871
NickCyrus <nicklemusduque@gmail.com>
2020-09-08 11:32:37 -0500

    Update

--- js/functions.js
+++ js/functions.js
@@ -11,7 +11,28 @@ function onDeviceReady() {
     StatusBar.backgroundColorByHexString('#999999');  
     navigator.splashscreen.hide();
     
-
+    window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+    var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+    // Set your iOS Settings
+    var iosSettings = {};
+    iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+    iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+    
+    window.plugins.OneSignal
+        .startInit("fec87477-319d-418a-a52e-3f736b283784") 
+        .handleNotificationOpened(notificationOpenedCallback)
+        .iOSSettings(iosSettings)
+        .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+        .endInit();
+    
+    // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+    window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+        console.log("User accepted notifications: " + accepted);
+    });
+ 
 }
 
 
